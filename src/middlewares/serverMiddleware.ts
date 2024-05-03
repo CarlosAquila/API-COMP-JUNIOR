b8b8da66d2c85127b3355780e56bd79c0c494929
perninha <carlos.aquila@hotmail.com>
2024-05-03 19:40:34 -0300

    🌟 feat: Adicionar middleware de erro

--- src/middlewares/serverMiddleware.ts
+++ src/middlewares/serverMiddleware.ts
@@ -1,1 +1,14 @@
-// to do: add serverMiddleware here
+import express from 'express';
+
+export function errorHandler(err: Error, request: express.Request, response: express.Response, next: express.NextFunction) {
+    if (err instanceof Error) {
+        return response.status(400).json({
+            error: err.message
+        });
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import { errorHandler } from './middlewares/serverMiddleware';
 
 const port = 3000;
 const app = express();
 
 // to do: add routes
 
-// to do: add error handling middleware
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
