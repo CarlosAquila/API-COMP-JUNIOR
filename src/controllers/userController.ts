5e301b80bd837a3caf6bb932ae1f0a0aa3fa7a39
perninha <carlos.aquila@hotmail.com>
2024-05-04 07:24:22 -0300

    🌟 feat: Adicionando validator DTO no Crud

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,12 +1,22 @@
 import { Request, Response } from "express";
 import { UserService } from "../services/userService";
+import UserDTO from "../dtos/userDTO";
 
 const userService = new UserService();
 
 export class UserController {
+
   async createUser(req: Request, res: Response) {
-    const { name, email, password, address } = req.body;
-    const newUser = await userService.createUser({ name, email, password, address});
-    return res.status(201).json(newUser);
+    try {
+      const userData: UserDTO = new UserDTO(req.body);
+      const newUser = await userService.createUser(userData);
+      return res.status(201).json(newUser);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return res.status(400).json({ error: error.message });
+      }
+      return res.status(500).json({ error: "Internal server error" });
+    }
   }
+
 }
