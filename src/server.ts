7fbfb6eeded354621681ff7164a6354472bd019b
perninha <carlos.aquila@hotmail.com>
2024-05-04 06:18:18 -0300

    :books: docs: Organiza arquivos de comando utilizados

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { errorHandler } from './middlewares/serverMiddleware';
-
 const port = 3000;
+
 const app = express();
+
+app.use(express.json());
 
 // to do: add routes
 
